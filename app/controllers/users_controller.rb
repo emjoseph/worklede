9ff7f6d7a1814404e452b360d143a5079c21cf9c
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-03-05 18:53:48 -0500

    Deleted resume creation from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,48 +27,4 @@ class UsersController < ApplicationController
     @user = User.find(id=params[:id])
   end
 
-  def update()
-    @user = User.find(id=session[:user_id])
-    @resume = @user.resumes.create()
-    @resume.name = params[:resume_name]
-
-    resume_file = params[:user][:resume_file]
-
-    # 1. Upload file to /public/uploads
-    fileUploadPath = Rails.root.join('public', 'uploads', resume_file.original_filename)
-
-    File.open(fileUploadPath, 'wb') do |file|
-      file.write(resume_file.read)
-
-      # 2. Upload file to worklede s3 bucket
-      s3 = Aws::S3::Resource.new(
-            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_KEY']),
-            region: 'us-east-1'
-      )
-
-      s3FileExtension = resume_file.original_filename.split('.')[1]
-      s3FileName = SecureRandom.uuid + "-resume." + s3FileExtension
-
-      s3Object = s3.bucket('worklede').object(s3FileName)
-      s3Object.upload_file(fileUploadPath, acl:'public-read')
-
-      # 3. Save resume s3 link on DB
-      @resume.s3_link = s3Object.public_url
-      @resume.save
-
-      # 4. Delete file from public/uploads
-      File.delete(fileUploadPath) if File.exist?(fileUploadPath)
-      puts("Upload done!")
-
-      redirect_to "/"
-    end
-  end
-
-  private
-    def resume_params
-      puts "RESUME PARAMS METHOD"
-      puts params
-      params.require(:user).permit(:resume_file, :resume_name)
-    end
-
 end
