ff9d3ff2ec2392ff3eb6d549e8527485c6b59140
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-03-04 23:07:36 -0500

    S3 uploads and db updates implemented without gross active storage

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.7.0'
 
 gem 'omniauth-linkedin-oauth2'
 gem 'aws-sdk-s3', require: false
+gem 'aws-sdk'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 # Use sqlite3 as the database for Active Record
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 class UsersController < ApplicationController
 
   def index
@@ -29,18 +31,56 @@ class UsersController < ApplicationController
   end
 
   def update()
-    puts "UPDATE USER"
-    puts params
-    @user = User.find(id=session[:user_id])
 
-    @resume = @user.resumes.create(resume_params)
-    @resume.resume_file.attach(params[:user][:resume_file])
-    @resume.save
-    puts @resume.resume_file.blob.key
-    puts "https://worklede.s3.amazonaws.com/#{@resume.resume_file.blob.key}"
-    puts "SAVE"
-    puts @resume.resume_file.attached?
-    puts @resume.to_yaml  end
+    @user = User.find(id=session[:user_id])
+    @resume = @user.resumes.create()
+
+    resume_file = params[:user][:resume_file]
+
+    # 1. Upload file to /public/uploads
+    fileUploadPath = Rails.root.join('public', 'uploads', resume_file.original_filename)
+    puts "File upload path"
+    puts(fileUploadPath)
+    puts(resume_file)
+    File.open(fileUploadPath, 'wb') do |file|
+      file.write(resume_file.read)
+
+      # 2. Upload file to worklede s3 bucket
+      s3 = Aws::S3::Resource.new(
+            credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_KEY']),
+            region: 'us-east-1'
+      )
+
+      s3FileExtension = resume_file.original_filename.split('.')[1]
+      s3FileName = SecureRandom.uuid + "-resume." + s3FileExtension
+
+      s3Object = s3.bucket('worklede').object(s3FileName)
+      s3Object.upload_file(fileUploadPath, acl:'public-read')
+
+      # 3. Save resume s3 link on DB
+      @resume.resume_link = s3Object.public_url
+      @resume.save
+
+      # 4. Delete file from public/uploads
+      File.delete(fileUploadPath) if File.exist?(fileUploadPath)
+      puts("Upload done!")
+
+      redirect_to "/"
+
+    end
+
+    #@resume.resußme_file.attach(params[:user][:resume_file])
+    #@resume.save
+    #puts @resume.resume_file.blob.key
+    #puts "https://worklede.s3.amazonaws.com/#{@resume.resume_file.blob.key}"
+    #puts "SAVE"
+    #puts @resume.resume_file.attached?
+    #puts @resume.to_yaml
+
+
+    #obj = AMAZON_S3_CLIENT('worklede').object('key')
+
+  end
 
 
   private
