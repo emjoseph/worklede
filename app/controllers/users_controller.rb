fd9c51810afcd753f72e0d88eeb44accf4d26b74
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-03-04 19:08:28 -0500

    S3 active storage working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,4 +28,26 @@ class UsersController < ApplicationController
     @user = User.find(id=params[:id])
   end
 
+  def update()
+    puts "UPDATE USER"
+    puts params
+    @user = User.find(id=session[:user_id])
+
+    @resume = @user.resumes.create(resume_params)
+    @resume.resume_file.attach(params[:user][:resume_file])
+    @resume.save
+    puts @resume.resume_file.blob.key
+    puts "https://worklede.s3.amazonaws.com/#{@resume.resume_file.blob.key}"
+    puts "SAVE"
+    puts @resume.resume_file.attached?
+    puts @resume.to_yaml  end
+
+
+  private
+    def resume_params
+      puts "RESUME PARAMS METHOD"
+      puts params
+      params.require(:user).permit(:resume_file)
+    end
+
 end
