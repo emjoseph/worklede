ee6367586392fd73eb67f78cd50f6a0c2226f793
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-04-03 12:25:09 -0400

    More updates

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, ENV['LINKEDIN_CLIENT_ID'], ENV['LINKEDIN_CLIENT_SECRET']
   puts 'Initialized OmniAuth LinkedIn'
+  puts ENV['HEROKU_DB_NAME']
+  puts ENV['HEROKU_DB_PASSWORD']
+  puts ENV['HEROKU_DB_NAME']
+  puts ENV['MAILGUN_USERNAME']
+  puts ENV['MAILGUN_PASSWORD']
+  puts ENV['AWS_SES_SMTP_USERNAME']
+
 end
