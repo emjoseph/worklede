abdfbc879c1063335b35549e88de20635a83dbbd
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-04-02 23:58:47 -0400

    Workday Cracked

--- app/scrapers/nyt.rb
+++ app/scrapers/nyt.rb
@@ -1,11 +1,30 @@
 require 'watir'
 
-def scrape_jobs_for_section(section_url, browser)
-  browser.goto section_url
+def scrape_jobs_for_section(url, section_name, company, platform, browser)
+
+  require 'json'
+  headers = {
+    "Accept"  => "application/json,application/xml"
+  }
+
+  jsonResponse = HTTParty.get(
+    url,
+    :headers => headers
+  )
+
+  itemUrls = []
+
+  linkJson = JSON.parse(jsonResponse.body)
+  linkItems =  linkJson['body']['children'][0]['children'][0]['listItems']
+  linkItems.each { |item|
+      itemUrls.append(item['title']['commandLink'])
+  }
+
+  browser.goto url
   sleep 2
 
   page = Nokogiri::HTML.parse(browser.html)
-  page.css('.WN0F').each do |posting|
+  page.css('.WN0F').each_with_index do |posting, index|
 
       title = posting.css('.WI2O')[1].text
       subheader = posting.css('span.WB1F').text
@@ -13,12 +32,15 @@ def scrape_jobs_for_section(section_url, browser)
       location = subheader.to_s.split('|')[1].strip
       time = subheader.to_s.split('|')[2].strip
       time_int = time.scan(/\d+/)[0].to_i
-      category = browser.url.split("/").last
 
       # Build job link from modifications to the scraped params above
       location_mod = location.tr(',','').gsub(/[^a-z]/i, '-')
       title_mod = title.gsub(/[^a-z]/i, '-')
-      job_url = "https://nytimes.wd5.myworkdayjobs.com/en-US/#{category}/job/#{location_mod}/#{title_mod}_#{posting_code}"
+      #job_url = "https://nytimes.wd5.myworkdayjobs.com/en-US/#{section_name}/job/#{location_mod}/#{title_mod}_#{posting_code}"
+      #if posting_code.split("").last(2)[0] != "-"
+      #  job_url = job_url+"-1"
+      #end
+      job_url = "https://nytimes.wd5.myworkdayjobs.com#{itemUrls[index]}"
 
       # Scrape the description text from the job url
       browser.goto job_url
@@ -42,17 +64,18 @@ def scrape_jobs_for_section(section_url, browser)
       job.desc = job_desc
       job.title = title
       job.code = posting_code
-      job.company = "The New York Times"
-      job.platform = "Workday"
+      job.company = company
+      job.platform = platform
       job.posted_days_ago_string = time
       job.posted_days_ago_int = time_int
-      job.category = category
+      job.category = section_name
       job.location = location
 
+      puts '************'
       puts job.url
       puts job.desc
-      puts job.company
       puts job.category
+      puts '************'
 
       # Save Job if not already in database
       if Job.where(:code => job.code, :company => job.company).blank?
@@ -67,14 +90,19 @@ end
 browser = Watir::Browser.new :chrome, headless: true
 
 jobs_section_urls = [
-  "https://nytimes.wd5.myworkdayjobs.com/marketing",
-  "https://nytimes.wd5.myworkdayjobs.com/Tech",
-  "https://nytimes.wd5.myworkdayjobs.com/News",
-  "https://nytimes.wd5.myworkdayjobs.com/DataInsights"
+  #["https://nytimes.wd5.myworkdayjobs.com/Marketing", "Marketing", "The New York Times", "Workday"]
+  ["https://nytimes.wd5.myworkdayjobs.com/Tech", "Tech", "The New York Times", "Workday"]
+  #["https://nytimes.wd5.myworkdayjobs.com/News", "News", "The New York Times", "Workday"],
+  #["https://nytimes.wd5.myworkdayjobs.com/DataInsights", "Data Insights", "The New York Times", "Workday"]
 ]
 
-jobs_section_urls.each { |section_url|
-   section = section_url.split("/").last
-   puts "Scraping jobs for #{section}"
-   scrape_jobs_for_section(section_url, browser)
+jobs_section_urls.each { |section_array|
+   url = section_array[0]
+   section_name = section_array[1]
+   company = section_array[2]
+   platform = section_array[3]
+
+   puts "Scraping jobs for #{section_name}"
+   scrape_jobs_for_section(url, section_name, company, platform, browser)
+   puts "----------------------------"
 }
