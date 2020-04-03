a6fe3db8865b582dcd9c38dcd6312dda3b4718db
Eugene M. Joseph <joseph.eugene@gmail.com>
2020-04-03 15:48:33 -0400

    Fixed issue where users could see each other's profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
 
   def index
     # If not logged in redirect to homepage
+    puts params
+    puts session[:user_id]
     @logged_in_user_id = session[:user_id]
     if @logged_in_user_id
       redirect_to user_path({:id => @logged_in_user_id})
@@ -23,8 +25,11 @@ class UsersController < ApplicationController
   end
 
   def show()
-    puts params
-    @user = User.find(id=params[:id])
+    if session[:user_id].to_i == params[:id].to_i
+      @user = User.find(id=params[:id])
+    else
+      redirect_to "/"
+    end
   end
 
 end
